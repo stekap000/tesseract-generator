3244e49f0df7f7d619e19dd35e94e631c5e9b1ad
stekap000 <stekap000@gmail.com>
2024-03-23 01:20:38 +0100

    Comments and full triangle generation

--- tesseract_generator.c
+++ tesseract_generator.c
@@ -2,17 +2,17 @@
 #include "stdbool.h"
 
 typedef struct {
+	// Values are always 0 or 1.
 	int x, y, z, w;
 } Point;
 
-int dist(Point p, Point q) {
-	return (p.x ^ q.x) + (p.y ^ q.y) + (p.z ^ q.z) + (p.w ^ q.w);
-}
-
+// Turn point into its index that we use as reference when writing indices.
 int point_index(Point p) {
 	return 8*p.x + 4*p.y + 2*p.z + p.w;
 }
 
+// Vertices are just variations of 4 elements with 2 possible values.
+// Each row contains data for one point.
 void generate_vertices() {
 	printf("float tesseract_vertices[16*4] = {\n");
 	for(int i = 0; i < 2; ++i) {
@@ -28,14 +28,18 @@ void generate_vertices() {
 	printf("};\n");
 }
 
+// For this point, find its direct neighbours and then form indices for
+// triangles that are made up of these points.
 void generate_point_indices(Point p0) {
+	// Adjacent points are all those that are different in one bit.
+	// (all those whose Hamming distance from current one is 1)
 	Point adjacent_points[4] = {
 		{p0.x ^ 1, p0.y, p0.z, p0.w},
 		{p0.x, p0.y ^ 1, p0.z, p0.w},
 		{p0.x, p0.y, p0.z ^ 1, p0.w},
 		{p0.x, p0.y, p0.z, p0.w ^ 1},
 	};
-	
+
 	for(int i = 0; i < 4; ++i) {
 		for(int j = i + 1; j < 4; ++j) {
 				printf("\t%d, %d, %d,\n",
@@ -47,8 +51,13 @@ void generate_point_indices(Point p0) {
 }
 
 void generate_indices() {
-	printf("int tesseract_indices[48*3] = {\n");
-	for(int i = 0; i < 16; i += 2)
+	printf("int tesseract_indices[96*3] = {\n");
+	// We are generating all possible triangles. This means 4 triangles per
+	// one face instead of 2.
+	// If there is a need for different triangle number per face generation,
+	// only this part would need to be adjusted (index skipping according to
+	// some arbitrary rule based on point distance).
+	for(int i = 0; i < 16; ++i)
 		generate_point_indices((Point){(i&1), (i&2) >> 1, (i&4) >> 2, (i&8) >> 3});
 	printf("};\n");
 }
